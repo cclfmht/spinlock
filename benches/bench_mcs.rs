070b2a59e75602b5e63f37063a69bf35c305ad03
cclfmht <chunchilin0202@gmail.com>
2026-08-29 10:15:33 +0800

    Benchmark under different number of threads
    Run benchmark for 1, 2, 4, 8, 16 threads.

--- benches/bench_mcs.rs
+++ benches/bench_mcs.rs
@@ -3,14 +3,14 @@ use spinlock::{McsLock, McsNode};
 use std::sync::Mutex;
 use std::thread;
 
-const NUM_THREADS: u8 = 16;
+const NUMS_THREADS: [u8; 5] = [1, 2, 4, 8, 16];
 const NUM_ITERS_PER_THREAD: u32 = 10000;
 
-fn mcs_shared_counter() {
+fn mcs_shared_counter(num_thread: u8) {
     let mcs = McsLock::new(0);
 
     thread::scope(|s| {
-        for _ in 0..NUM_THREADS {
+        for _ in 0..num_thread {
             s.spawn(|| {
                 let mut node = McsNode::new();
                 for _ in 0..NUM_ITERS_PER_THREAD {
@@ -21,11 +21,11 @@ fn mcs_shared_counter() {
     })
 }
 
-fn mutex_shared_counter() {
+fn mutex_shared_counter(num_thread: u8) {
     let m = Mutex::new(0);
 
     thread::scope(|s| {
-        for _ in 0..NUM_THREADS {
+        for _ in 0..num_thread {
             s.spawn(|| {
                 for _ in 0..NUM_ITERS_PER_THREAD {
                     *m.lock().unwrap() += 1;
@@ -36,14 +36,19 @@ fn mutex_shared_counter() {
 }
 
 fn bench_mcs_and_mutex(c: &mut Criterion) {
-    let mut group = c.benchmark_group("MCS lock and Mutex");
-    group.bench_function(BenchmarkId::new("MCS lock on shared counter", 0), |b| {
-        b.iter(|| mcs_shared_counter())
-    });
-    group.bench_function(BenchmarkId::new("Mutex on shared counter", 1), |b| {
-        b.iter(|| mutex_shared_counter())
-    });
-    group.finish();
+    for num_thread in NUMS_THREADS {
+        let mut group = c.benchmark_group(format!(
+            "MCS lock and Mutex on shared counter - {} thread(s)",
+            num_thread
+        ));
+        group.bench_function(BenchmarkId::new("MCS lock", ""), |b| {
+            b.iter(|| mcs_shared_counter(num_thread))
+        });
+        group.bench_function(BenchmarkId::new("Mutex", ""), |b| {
+            b.iter(|| mutex_shared_counter(num_thread))
+        });
+        group.finish();
+    }
 }
 
 criterion_group!(benches, bench_mcs_and_mutex);
